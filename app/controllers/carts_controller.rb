c4f71fdd2de6bbf9997e5b46fcaca25a062630ca
Martin Hynes <martinhynesone@gmail.com>
2019-11-18 14:20:32 +0000

    Added error handling

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,5 @@
 class CartsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
 
   # GET /carts
@@ -62,6 +63,10 @@ class CartsController < ApplicationController
   end
 
   private
+    def invalid_cart
+	  logger.error "Attempt to access invalid cart #{params[:id]}"
+	  redirect_to store_url, notice: 'Invalid cart'
+	end
     # Use callbacks to share common setup or constraints between actions.
     def set_cart
       @cart = Cart.find(params[:id])
