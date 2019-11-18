c5dd66fe2804e938e0e5d64e4e56b6f851b53eb5
Martin Hynes <martinhynesone@gmail.com>
2019-11-18 13:37:37 +0000

    added quantity column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_122100) do
+ActiveRecord::Schema.define(version: 2019_11_18_132820) do
 
   create_table "cards", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_11_18_122100) do
     t.integer "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
   end
