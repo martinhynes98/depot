fc9aeefe37587f1d2b982c4c41fbe3c83bfa1da2
Martin Hynes <martinhynesone@gmail.com>
2019-11-18 12:20:25 +0000

    Created Concern File

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :carts
+  resources :cards
   root to: 'store#index', as: 'store'
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_03_162956) do
+ActiveRecord::Schema.define(version: 2019_11_18_121433) do
+
+  create_table "cards", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
